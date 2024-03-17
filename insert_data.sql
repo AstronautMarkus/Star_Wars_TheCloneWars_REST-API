18825fbf359b58c147097284a9b9d9fbb5a258e8
Marcos Reyes <marco.reyesm@duocuc.cl>
2024-03-16 23:43:06 -0300

    added database creation and insert data scripts SQL

---
+++ insert_data.sql
@@ -0,0 +1,168 @@
+# Season 1
+
+INSERT INTO CLONE_WARS_QUOTES (quote, season, chapter) VALUES
+('Great leaders inspire greatness in others.', 1, 1),
+('Belief is not a matter of choice, but of conviction.', 1, 2),
+('Easy is the path to wisdom for those not blinded by ego.', 1, 3),
+('A plan is only as good as those who see it through.', 1, 4),
+('The best confidence builder is experience.', 1, 5),
+('Trust in your friends, and they’ll have reason to trust in you.', 1, 6),
+('You hold onto friends by keeping your heart a little softer than your head.', 1, 7),
+('Heroes are made by the times.', 1, 8),
+('Ignore your instincts at your peril.', 1, 9),
+('Most powerful is he who controls his own power.', 1, 10),
+('Great leaders inspire greatness in others.', 1, 1),
+('Belief is not a matter of choice, but of conviction.', 1, 2),
+('Easy is the path to wisdom for those not blinded by ego.', 1, 3),
+('A plan is only as good as those who see it through.', 1, 4),
+('The best confidence builder is experience.', 1, 5),
+('Trust in your friends, and they’ll have reason to trust in you.', 1, 6),
+('You hold onto friends by keeping your heart a little softer than your head.', 1, 7),
+('Heroes are made by the times.', 1, 8),
+('Ignore your instincts at your peril.', 1, 9),
+('Most powerful is he who controls his own power.', 1, 10),
+('The winding path to peace is always a worthy one, regardless of how many turns it takes.', 1, 11),
+('Fail with honor rather than succeed by fraud.', 1, 12),
+('Greed and fear of loss are the roots that lead to the tree of evil.', 1, 13),
+('When surrounded by war, one must eventually choose a side.', 1, 14),
+('Arrogance diminishes wisdom.', 1, 15),
+('Truth enlightens the mind, but won’t always bring happiness to your heart.', 1, 16),
+('Fear is a disease; hope is its only cure.', 1, 17),
+('A single chance is a galaxy of hope.', 1, 18),
+('It is a rough road that leads to the heights of greatness.', 1, 19),
+('The costs of war can never be truly accounted for.', 1, 20),
+('Compromise is a virtue to be cultivated, not a weakness to be despised.', 1, 21),
+('A secret shared is a trust formed.', 1, 22);
+
+
+# Season 2
+
+INSERT INTO CLONE_WARS_QUOTES (quote, season, chapter) VALUES
+('A lesson learned is a lesson earned.', 2, 1),
+('Overconfidence is the most dangerous form of carelessness.', 2, 2),
+('The first step to correcting a mistake is patience.', 2, 3),
+('Believe in yourself or no one else will.', 2, 5),
+('No gift is more precious than trust.', 2, 6),
+('Sometimes, accepting help is harder than offering it.', 2, 7),
+('Attachment is not compassion.', 2, 8),
+('For everything you gain, you lose something else.', 2, 9),
+('It is the quest for honor that makes one honorable.', 2, 10),
+('Easy isn’t always simple.', 2, 11),
+('If you ignore the past, you jeopardize the future.', 2, 12),
+('Fear not for the future, weep not for the past.', 2, 13),
+('In war, truth is the first casualty.', 2, 14),
+('Searching for the truth is easy. Accepting the truth is hard.', 2, 15),
+('A wise leader knows when to follow.', 2, 16),
+('Courage makes heroes, but trust builds friendships.', 2, 17),
+('Choose what is right, not what is easy.', 2, 18),
+('The most dangerous beast is the beast within.', 2, 19),
+('Who my father was matters less than my memory of him.', 2, 20),
+('Adversity is a friendship’s truest test.', 2, 21),
+('Revenge is a confession of pain.', 2, 22);
+
+# Season 3
+
+INSERT INTO CLONE_WARS_QUOTES (quote, season, chapter) VALUES
+('Brothers in arms are brothers for life.', 3, 1),
+('Fighting a war tests a soldier’s skills, defending his home tests a soldier’s heart.', 3, 2),
+('Where there’s a will, there’s a way.', 3, 3),
+('Sphere of Influence: A child stolen is a hope lost.', 3, 4),
+('The challenge of hope is to overcome corruption.', 3, 5),
+('Those who enforce the law must obey the law.', 3, 6),
+('The future has many paths - choose wisely.', 3, 7),
+('A failure in planning is a plan for failure.', 3, 8),
+('Love comes in all shapes and sizes.', 3, 9),
+('Fear is a great motivator.', 3, 10),
+('Truth can strike down the spectre of fear.', 3, 11),
+('The swiftest path to destruction is through vengeance.', 3, 12),
+('Evil is not born, it is taught.', 3, 13),
+('The path to evil may bring great power, but not loyalty.', 3, 14),
+('Balance is found in the one who faces his guilt.', 3, 15),
+('He who surrenders hope, surrenders life.', 3, 16),
+('Who seeks to control fate shall never find peace.', 3, 17),
+('Adaptation is the key to survival.', 3, 18),
+('Anything that can go wrong will.', 3, 19),
+('Without honor, victory is hollow.', 3, 20),
+('Without humility, courage is a dangerous game.', 3, 21),
+('A great student is what the teacher hopes to be.', 3, 22);
+
+# Season 4
+
+INSERT INTO CLONE_WARS_QUOTES (quote, season, chapter) VALUES
+('When destiny calls, the chosen have no choice.', 4, 1),
+('Only through fire is a strong sword forged.', 4, 2),
+('Crowns are inherited, kingdoms are earned.', 4, 3),
+('Who a person truly is cannot be seen with the eye.', 4, 4),
+('Understanding is honoring the truth beneath the surface.', 4, 5),
+('Who’s the more foolish, the fool or the fool who follows him?', 4, 6),
+('The first step towards loyalty is trust.', 4, 7),
+('The path of ignorance is guided by fear.', 4, 8),
+('The wise man leads, the strong man follows.', 4, 9),
+('Our actions define our legacy.', 4, 10),
+('Where we are going always reflects where we came from.', 4, 11),
+('Those who enslave others, inevitably become slaves themselves.', 4, 12),
+('Great hope can come from small sacrifices.', 4, 13),
+('Friendship shows us who we really are.', 4, 14),
+('All warfare is based on deception.', 4, 15),
+('Keep your friends close, but keep your enemies closer.', 4, 16),
+('The strong survive, the noble overcome.', 4, 17),
+('Trust is the greatest of gifts, but it must be earned.', 4, 18),
+('One must let go of the past to hold on to the future.', 4, 19),
+('Who we are never changes, who we think we are does.', 4, 20),
+('A fallen enemy may rise again, but the reconciled one is truly vanquished.', 4, 21),
+('The enemy of my enemy is my friend.', 4, 22);
+
+# Season 5
+
+INSERT INTO CLONE_WARS_QUOTES (quote, season, chapter) VALUES
+('Strength of character can defeat strength in numbers.', 5, 1),
+('Fear is a malleable weapon.', 5, 2),
+('To seek something is to believe in its possibility.', 5, 3),
+('Struggles often begin and end with the truth.', 5, 4),
+('Disobedience is a demand for change.', 5, 5),
+('He who faces himself, finds himself.', 5, 6),
+('The young are often underestimated.', 5, 7),
+('When we rescue others, we rescue ourselves.', 5, 8),
+('Choose your enemies wisely, as they may be your last hope.', 5, 9),
+('Humility is the only defense against humiliation.', 5, 10),
+('When all seems hopeless, a true hero gives hope.', 5, 11),
+('A soldier’s most powerful weapon is courage.', 5, 12),
+('You must trust in others or success is impossible.', 5, 13),
+('One vision can have many interpretations.', 5, 14),
+('Alliances can stall true intentions.', 5, 15),
+('Morality separates heroes from villains.', 5, 16),
+('Sometimes even the smallest doubt can shake the greatest belief.', 5, 17),
+('Courage begins by trusting oneself.', 5, 18),
+('Never become desperate enough to trust the untrustworthy.', 5, 19),
+('Never give up hope, no matter how dark things seem.', 5, 20);
+
+# Season 6
+
+INSERT INTO CLONE_WARS_QUOTES (quote, season, chapter) VALUES
+('The truth about yourself is always the hardest to accept.', 6, 1),
+('The wise benefit from a second opinion.', 6, 2),
+('When in doubt, go to the source.', 6, 3),
+('The popular belief isn’t always the correct one.', 6, 4),
+('To love, is to trust. To trust is to believe.', 6, 5),
+('Jealousy is the path to chaos.', 6, 6),
+('Deceit is the weapon of greed.', 6, 7),
+('Without darkness there cannot be light.', 6, 8),
+('Wisdom is born in fools as well as wise men.', 6, 9),
+('What is lost is often found.', 6, 10),
+('Madness can sometimes be the path to truth.', 6, 11),
+('Death is just the beginning.', 6, 12),
+('Facing all that you fear will free you from yourself.', 6, 13);
+
+#Season 7
+
+INSERT INTO CLONE_WARS_QUOTES (quote, season, chapter) VALUES
+('Embrace others for their differences, for that makes you whole.', 7, 1),
+('The search for truth begins with belief.', 7, 2),
+('Survival is one step on the path to living.', 7, 3),
+('Trust placed in another is trust earned.', 7, 4),
+('If there is no path before you, create your own.', 7, 5),
+('Mistakes are valuable lessons often learned too late.', 7, 6),
+('Who you were does not have to define who you are.', 7, 7),
+('You can change who you are, but you cannot run from yourself.', 7, 8);
+
+
